ffbb5484fd7ed8d828d025a2c9c10f5a39b76916
Muhammad Yusuf Rizal <myusufrizal34@gmail.com>
2021-11-03 20:20:48 +0700

    First Commit

---
+++ Assets/Scripts/CharacterControllers.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllers : MonoBehaviour
+{
+    private CharacterController controller;
+    public Position positions =  Position.Center;
+    private float newPosition = 2f;
+    private float currentPosition;
+    
+    [SerializeField] private float dodgeSpeed;
+    [SerializeField] private float speed;
+    
+    private void Start(){
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update() {
+        if(Input.GetKeyDown("left")){
+            if(positions != Position.Left){
+                if(positions == Position.Center){
+                    currentPosition = -newPosition;
+                    positions = Position.Left;
+                }
+                else if(positions == Position.Right){
+                    currentPosition = 0;
+                    positions = Position.Center;
+                }
+            }
+        }
+
+        if(Input.GetKeyDown("right")){
+            if(positions != Position.Right){
+                if(positions == Position.Center){
+                    currentPosition = newPosition;
+                    positions = Position.Right;
+                }
+                else if(positions == Position.Left){
+                    currentPosition = 0;
+                    positions = Position.Center;
+                }
+            }
+        }
+
+        Vector3 moving = new Vector3((currentPosition - transform.position.x) * dodgeSpeed * Time.deltaTime, 0, speed * Time.deltaTime);
+        controller.Move(moving);
+    }
+}
+
+public enum Position{
+    Right,
+    Center,
+    Left
+}
