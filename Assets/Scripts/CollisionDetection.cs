3dafe35e1da0a2217231c17167c647dab07d853d
Muhammad Yusuf Rizal <myusufrizal34@gmail.com>
2021-11-26 21:52:20 +0700

    Add swipe up (jump)

--- Assets/Scripts/CollisionDetection.cs
+++ Assets/Scripts/CollisionDetection.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class CollisionDetection : MonoBehaviour
 {
-    private GameObject parent;
+    // private GameObject parent;
+    private Rigidbody rb;
+    public float force;
 
-    // private void OnCollisionEnter(Collision other) {
-    //     if(other.gameObject.tag == "Obstacle"){
-    //         print(gameObject);
-    //         gameObject.GetComponentInParent<CharacterControllers>().StumbleController(gameObject);
-    //     }
-    // }
+    private void Awake() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.tag == "Player"){
+            // print("yes");
+            // gameObject.GetComponentInParent<CharacterControllers>().StumbleController(gameObject);
+            rb.AddForce(transform.forward * force, ForceMode.Force);
+            rb.useGravity = true;
+        }
+    }
+
+    private void OnMouseDown() {
+        rb.AddForce(transform.forward * force, ForceMode.Force);
+        rb.useGravity = true;
+    }
 }
