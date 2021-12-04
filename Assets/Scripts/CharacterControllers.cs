3a5f35caac6e226cf5072b43ada87c9b9517e631
Muhammad Yusuf Rizal <myusufrizal34@gmail.com>
2021-12-04 20:44:05 +0700

    Update UI
    - Score
    - Coin

--- Assets/Scripts/CharacterControllers.cs
+++ Assets/Scripts/CharacterControllers.cs
@@ -31,19 +31,17 @@ public class CharacterControllers : MonoBehaviour
     public float speed;
     [SerializeField] private float dodgeSpeed;
 
+    [Header("ANDROID MOVEMENT CONTROLLER")]
+    public float tiltingSpeed;
+    [SerializeField] private float tiltingDodgeSpeed;
+
     [Header("GRAVITY SETTING")]
     public float gravity = -0.5f;
-
-    [Header("PLAYER DISTANCE")]
-    public Text distancemove;
-    float distanceunit = 0;
-    public string display = "Score : {0}";
 
     private void Start()
     {
         _controller = GetComponent<CharacterController>();
         SetupJump();
-        InvokeRepeating("distance", 0, 1 / speed);
     }
 
     private void Update()
@@ -54,70 +52,85 @@ public class CharacterControllers : MonoBehaviour
 
     private void MovementController()
     {
+        Vector3 moving;
+        
         //USING ANDROID
         if(isAndroid){
-            currentXPosition = Input.acceleration.x;
-
-            //CHECK IF TOUCHED
-            if (Input.touches.Length > 0)
-            {
-                if (Input.touches[0].phase == TouchPhase.Began)
-                {
-                    tap = true;
-                    isDraging = true;
-                    startTouch = Input.touches[0].position;
-                }
-                else if (Input.touches[0].phase == TouchPhase.Ended || Input.touches[0].phase == TouchPhase.Canceled)
-                {
-                    isDraging = false;
-                    Reset();
-                }
-            }
-
-            swipeDelta = Vector2.zero;
-            if (isDraging)
-            {
-                // if (Input.touches.Length < 0)
-                //     swipeDelta = Input.touches[0].position - startTouch;
-                if (Input.GetMouseButton(0))
-                    swipeDelta = (Vector2)Input.mousePosition - startTouch;
-            }
-
-            if (swipeDelta.magnitude > 100)
-            {
-                float x = swipeDelta.x;
-                float y = swipeDelta.y;
-                if (Mathf.Abs(x) < Mathf.Abs(y))
-                {
-                    if (y > 0) currentYPosition = initialJumpVelocity;
-                }
-                Reset();
-            }
+            moving = AndroidMovement();
         }
         //USING KEYBOARD
         else{
-            currentXPosition = 0;
+            moving = KeyboardMovement();
+        }
 
-            if(Input.GetKey("left"))
-            {
-                currentXPosition = -speed;
-            }
+        _controller.Move(moving * Time.deltaTime);
+    }
 
-            if(Input.GetKey("right"))
-            {
-                currentXPosition = speed;
-            }
+    private Vector3 KeyboardMovement(){
+        currentXPosition = 0;
 
-            if(Input.GetKeyDown("space")){
-                if(_controller.isGrounded){
-                    currentYPosition = initialJumpVelocity;
-                }
+        if(Input.GetKey("left"))
+        {
+            currentXPosition = -speed;
+        }
+
+        if(Input.GetKey("right"))
+        {
+            currentXPosition = speed;
+        }
+
+        if(Input.GetKeyDown("space")){
+            if(_controller.isGrounded){
+                currentYPosition = initialJumpVelocity;
             }
         }
 
         Vector3 moving = new Vector3(currentXPosition * dodgeSpeed, currentYPosition, speed);
+        return moving;
+    }
 
-        _controller.Move(moving * Time.deltaTime);
+    private Vector3 AndroidMovement(){
+        currentXPosition = Input.acceleration.x;
+
+        //CHECK IF TOUCHED
+        if (Input.touches.Length > 0)
+        {
+            if (Input.touches[0].phase == TouchPhase.Began)
+            {
+                tap = true;
+                isDraging = true;
+                startTouch = Input.touches[0].position;
+            }
+            else if (Input.touches[0].phase == TouchPhase.Ended || Input.touches[0].phase == TouchPhase.Canceled)
+            {
+                isDraging = false;
+                Reset();
+            }
+        }
+
+        swipeDelta = Vector2.zero;
+
+        if (isDraging)
+        {
+            // if (Input.touches.Length < 0)
+            //     swipeDelta = Input.touches[0].position - startTouch;
+            if (Input.GetMouseButton(0))
+                swipeDelta = (Vector2)Input.mousePosition - startTouch;
+        }
+
+        if (swipeDelta.magnitude > 100)
+        {
+            float x = swipeDelta.x;
+            float y = swipeDelta.y;
+            if (Mathf.Abs(x) < Mathf.Abs(y))
+            {
+                if (y > 0) currentYPosition = initialJumpVelocity;
+            }
+            Reset();
+        }
+
+        Vector3 moving = new Vector3(currentXPosition * tiltingDodgeSpeed, currentYPosition, tiltingSpeed);
+        return moving;
     }
 
     private IEnumerator StumbleDelay(float delay)
@@ -143,12 +156,5 @@ public class CharacterControllers : MonoBehaviour
     {
         startTouch = swipeDelta = Vector2.zero;
         isDraging = false;
-    }
-
-    void distance()
-    {
-        display = "Score = {0}";
-        distanceunit = distanceunit + 1;
-        distancemove.text = distanceunit.ToString();
     }
 }
--- Assets/Scripts/Collectables/ScoreController.cs
+++ Assets/Scripts/Collectables/ScoreController.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CollactableCoin : MonoBehaviour
+public class ScoreController : MonoBehaviour
 {
-    public static int coinCount;
-    public Text coinText;
+    public static int distance;
+    public Text scoreText;
 
     // Update is called once per frame
     void Update()
     {
-        coinText.text = "" + coinCount;
+        scoreText.text = "" + (int)transform.position.z;
     }
 }
