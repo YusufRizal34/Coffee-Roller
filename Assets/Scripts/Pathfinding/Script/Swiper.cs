d572b183127d131c118db1d2515c4521cd632aee
Rofif-Anandya <61696512+Rofif-Anandya@users.noreply.github.com>
2022-06-27 20:30:36 +0700

    tambahan kamera

---
+++ Assets/Scripts/Pathfinding/Script/Swiper.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swiper : MonoBehaviour
+{
+    private Touch initTouch = new Touch();
+    public Camera cam;
+
+    private float rotX = 0f;
+    private float rotY = 0f;
+    private Vector3 origRot;
+
+    public float rotspeed = 5f;
+    public float dir = -1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        origRot = cam.transform.eulerAngles;
+        rotX = origRot.x;
+        rotY = origRot.y;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        foreach(Touch touch in Input.touches)
+        {
+            if(touch.phase == TouchPhase.Began)
+            {
+                initTouch = touch;
+            }
+            else if(touch.phase == TouchPhase.Moved)
+            {
+                float deltaX = initTouch.position.x - touch.position.x;
+                float deltaY = initTouch.position.y - touch.position.y;
+
+                rotX -= deltaY * Time.deltaTime * rotspeed * dir;
+                rotY += deltaX * Time.deltaTime * rotspeed * dir;
+                rotX = Mathf.Clamp(rotX, -20f, 20f);
+                rotY = Mathf.Clamp(rotY, -20f, 20f);
+
+                cam.transform.eulerAngles = new Vector3(rotX, rotY, 0f);
+            }
+            else if(touch.phase == TouchPhase.Ended)
+            {
+                initTouch = new Touch();
+            }
+        }
+    }
+}
